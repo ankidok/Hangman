15d0edda422df8f28b2cff94c785a441bb093ab8
Burak <34709493+ankidok@users.noreply.github.com>
2021-11-21 17:56:53 +0300

    Update hangmanalpha.py

--- hangmanalpha.py
+++ hangmanalpha.py
@@ -7,12 +7,13 @@ from sys import exit
 
 class Hangman:
     def __init__(self):
-        self.words = wordList
-        self.word = random.choice(self.words)
+        self.wordAndHint = random.choice(wordList)
+        self.word = self.wordAndHint[0]
+        self.hint = self.wordAndHint[1]
         self.spaces = list()
-        self.hint = ''
-        self.hintedSpaces = ""
-        self.hintsLeft = 8
+        self.guess = ''
+        self.guessedSpaces = ""
+        self.guessesLeft = 8
         self.control1 = 0
         self.control2 = 0
         self.menu()
@@ -28,14 +29,15 @@ class Hangman:
             system("cls")
             self.control1 = len(self.word)
             self.control2 = len(self.word)
-            self.hintedSpaces = ' '.join(self.spaces)
+            self.guessedSpaces = ' '.join(self.spaces)
             while True:
-                print(hangmanascii[-self.hintsLeft])
-                print(f"{self.hintsLeft} tane hakkınız kaldı")
-                print(self.hintedSpaces)
-                self.hint = input('\nTahminde bulunun: ').upper()
-                if self.hint.isalpha():
-                    self.hint = self.hint[0]
+                print(hangmanascii[-self.guessesLeft])
+                print(f"{self.guessesLeft} tane hakkınız kaldı")
+                print(self.guessedSpaces)
+                print("\n"+self.hint+"\n")
+                self.guess = input('\nTahminde bulunun: ').upper()
+                if self.guess.isalpha():
+                    self.guess = self.guess[0]
                     break
                 else:
                     print("\nLütfen bir karakter girin...")
@@ -43,28 +45,29 @@ class Hangman:
                     system('cls')
                     continue
             for i in range(0, len(self.word)):
-                if self.word[i] == self.hint:
-                    self.spaces[i] = self.hint
+                if self.word[i] == self.guess:
+                    self.spaces[i] = self.guess
                     self.control1 -= 1
                     continue
             else:
                 if not (self.control1 < self.control2):
-                    self.hintsLeft -= 1
+                    self.guessesLeft -= 1
 
             if self.spaces.count('_') == 0:
                 system("cls")
-                print(hangmanascii[-self.hintsLeft])
-                self.hintedSpaces = ' '.join(self.spaces)
-                print(self.hintedSpaces)
-                print("Kelimeyi buldunuz, tebrikler...")
+                print(hangmanascii[-self.guessesLeft])
+                self.guessedSpaces = ' '.join(self.spaces)
+                print(self.guessedSpaces)
+                print("\nKelimeyi buldunuz, tebrikler...")
                 sleep(1)
                 exit()
-            elif self.hintsLeft == 0:
+            elif self.guessesLeft == 0:
                 system("cls")
-                print(hangmanascii[-self.hintsLeft])
-                self.hintedSpaces = ' '.join(self.spaces)
-                print(self.hintedSpaces)
-                print("İstiklal mahkemelerinin kararıyla asıldın...\nBundan 100 sene sonra üzerinden rant yapılacak...")
+                print(hangmanascii[-self.guessesLeft])
+                self.guessedSpaces = ' '.join(self.spaces)
+                print(self.guessedSpaces)
+                print("\nİstiklal mahkemelerinin kararıyla asıldın...\nBundan 100 sene sonra üzerinden rant "
+                      "yapılacak...")
                 sleep(2)
                 exit()
             sleep(0.5)
